ed899442acaafbca73b2328725854a453aff3186
dirunru <2315198050@qq.com>
2019-03-08 16:26:41 +0800

    20190308 16:26 sass的整体样式提交

--- src/main.js
+++ src/main.js
@@ -3,19 +3,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Cookies from 'Cookies'
-
-Vue.prototype.Cookies = Cookies
-
+import './assets/sass/index.scss'
 import animated from  'animate.css'
-Vue.use(animated)
-
-import $ from 'jquery'
-
 import ElementUi from 'element-ui'
-Vue.use(ElementUi)
-
 import 'bootstrap3/dist/css/bootstrap.css'
 import 'bootstrap3/dist/js/bootstrap.js'
+import $ from 'jquery'
+
+
+Vue.prototype.Cookies = Cookies
+Vue.use(ElementUi)
+Vue.use(animated)
 
 Vue.config.productionTip = false
 
