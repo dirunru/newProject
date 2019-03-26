b22a64c203b7d773e503cc0123bab928223e9195
dirunru <2315198050@qq.com>
2019-03-26 15:53:11 +0800

    push

--- src/components/eleCalendar/eleCalendar.js
+++ src/components/eleCalendar/eleCalendar.js
@@ -1,35 +1,49 @@
 import eleCalendar from 'ele-calendar'
-import 'ele-calendar/dist/vue-calendar.css' ;
+import 'ele-calendar/dist/vue-calendar.css'
 export default {
-    data(){
-        return{
-            datedef:[
-                {"date":"2019-03-10","content":555,"cid":null},
-                {"date":"2019-03-15","content":666,"cid":null},
-            ],
-            prop:'date' //对应日期字段名
-        }
+  data() {
+    return {
+      datedef: [{
+          "date": "2019-03-10",
+          "content": 555,
+          "cid": null
+        },
+        {
+          "date": "2019-03-15",
+          "content": 666,
+          "cid": null
+        },
+      ],
+      prop: 'date' //对应日期字段名
+    }
+  },
+  components: {
+    eleCalendar
+  },
+  methods: {
+    renderContent(h, parmas) {
+      const loop = data => {
+        return (
+          data.defvalue.value ? ( < div > < div > {
+              data.defvalue.text
+            } < /div>  <
+            span style = "color:red" > {
+              "￥" + data.defvalue.value.content
+            } < /span> < /
+            div > ) : < div > {
+            data.defvalue.text
+          } < /div >
+        )
+      }
+      return ( <
+        div style = "min-height:60px;" > {
+          loop(parmas)
+        } <
+        /div>
+      );
     },
-    components: {
-        eleCalendar
-    },
-    methods: {
-      renderContent(h,parmas) {
-        const loop = data =>{
-          return (
-            data.defvalue.value ? (<div><div>{data.defvalue.text}</div> 
-            <span style="color:red">{"￥"+data.defvalue.value.content}</span>
-            </div>) : <div>{data.defvalue.text}</div>
-          )
-       }
-       return (
-        <div  style="min-height:60px;">
-         {loop(parmas)}
-        </div>
-        );
-     },
-     clickChange(data,event,row,dome){
-        console.log(data,event,row,dome)
-     }
-   }
+    clickChange(data, event, row, dome) {
+      console.log(data, event, row, dome)
+    }
+  }
 }
