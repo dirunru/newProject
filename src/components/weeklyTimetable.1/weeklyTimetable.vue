8814c4bab808b5d128aebd9d64b870e1728b1c8a
dirunru <2315198050@qq.com>
2019-04-21 19:18:56 +0800

    table

---
+++ src/components/weeklyTimetable.1/weeklyTimetable.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="weeklyTimetable">
+        <div class="left">
+            <table cellspacing="0">
+                <tr>
+                    <th width="100">周</th>
+                    <th width="120">日期</th>
+                </tr>
+                <tr v-for='(day,idx) in weekDays' :key='idx'>
+                    <td class="weekItem"><span>{{day.day}}</span><span class='date'>{{day.date}}</span></td>
+                    <td class="timeSlot">
+                        <p v-for='(timeItem,i) in day.timeSlot' :key="i">
+                            {{timeItem}}
+                        </p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import index from './index.js'
+    export default index
+</script>
+
+
+<style lang='scss' scoped>
+    @import './index.scss';
+</style>
---
+++ src/components/weeklyTimetable/index.js
@@ -0,0 +1,17 @@
+export default {
+    data() {
+        return {
+            weekItems:['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+        };
+    },
+
+    components: {},
+
+    computed: {},
+
+    created() {},
+
+    mounted() {},
+
+    methods: {}
+}
