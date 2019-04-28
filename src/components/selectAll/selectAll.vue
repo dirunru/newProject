d2dbab86c3f762e865c3bc5406cb58c2b89dfaae
dirunru <2315198050@qq.com>
2019-04-28 20:00:06 +0800

    清空

--- src/components/selectAll/selectAll.vue
+++ src/components/selectAll/selectAll.vue
@@ -1,21 +1,7 @@
 <template>
-    <div>
-    <span style="margin-left:30px;font-weight:bolder;">教练：
-        <el-select v-model="staffId" 
-          placeholder="请选择"
-          multiple
-          collapse-tags
-          @change="changeStaff"
-          style="width:180px">
-          <el-option
-            v-for="(item, key) in staff"
-            :key="key"
-            :label="item.nameCn"
-            :value="item.value">
-          </el-option>
-        </el-select>  
-      </span>
-    </div>
+  <div>
+    <h1> 好</h1>
+  </div>
 </template>
 
 
